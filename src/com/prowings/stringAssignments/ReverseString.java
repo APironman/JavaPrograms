ba41f4e65d2141bfd2b1cfe280f5a45d77bdb374
Atul-Patil <atulpatil051099@gmail.com>
2023-11-19 14:59:45 +0530

    Reverse String

---
+++ src/com/prowings/stringAssignments/ReverseString.java
@@ -0,0 +1,14 @@
+package com.prowings.stringAssignments;
+
+import java.util.Scanner;
+
+public class ReverseString {
+
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		String word = sc.next();
+		
+	}
+
+}
