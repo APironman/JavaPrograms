79fa0c8ed36eeee643f61905cb7e28659c224609
Atul-Patil <atulpatil051099@gmail.com>
2023-11-19 15:34:56 +0530

    Reverse String

--- src/com/prowings/stringAssignments/ReverseString.java
+++ src/com/prowings/stringAssignments/ReverseString.java
@@ -7,8 +7,36 @@ public class ReverseString {
 	static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
+		System.out.println("Enter any Word");
 		String word = sc.next();
-		
+		System.out.println("Reverse String using Buffer : " + reverseString1(word));
+		System.out.println("Reverse String using char[] : " + reverseString2(word));
+		System.out.println("Reverse String using another String : " + reverseString3(word));
+	}
+	
+	// Reverse String using String Buffer Method
+	private static String reverseString1(String word) {
+		StringBuffer sb = new StringBuffer(word);
+		sb.reverse();
+		String s = new String(sb);
+		return s;
+	}
+
+	// Reverse String using Char Array
+	private static String reverseString2(String word) {
+		char[] ch = word.toCharArray();
+		int j =0;
+		for(int i = word.length()-1;i>=0;i--)
+			ch[j++] = word.charAt(i);
+			return new String(ch);
+	}
+
+	// Reverse String using another String
+	private static String reverseString3(String word) {
+		String s = "";
+		for (int i = word.length() - 1; i >= 0; i--)
+			s += word.charAt(i);
+		return s;
 	}
 
 }
