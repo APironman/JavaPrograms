51ebb3e63d9397b471670d878479d49c11d7b8d8
Atul-Patil <atulpatil051099@gmail.com>
2023-11-20 10:02:56 +0530

    Remove Spaces

---
+++ src/com/prowings/stringAssignments/RemoveWhiteSpaces.java
@@ -0,0 +1,19 @@
+package com.prowings.stringAssignments;
+
+import java.util.Scanner;
+
+public class RemoveWhiteSpaces {
+
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		String sentence = sc.nextLine();
+		System.out.println("After Removing Spaces : "+removeSpaces(sentence));
+
+	}
+
+	public static String removeSpaces(String sentence) {
+		
+		return null;
+	}
+}
