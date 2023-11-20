d9ce796976f44f8575013bf7daf3b3d6362ac3a0
Atul-Patil <atulpatil051099@gmail.com>
2023-11-20 10:25:03 +0530

    Remove Spaces

--- src/com/prowings/stringAssignments/RemoveWhiteSpaces.java
+++ src/com/prowings/stringAssignments/RemoveWhiteSpaces.java
@@ -8,18 +8,20 @@ public class RemoveWhiteSpaces {
 
 	public static void main(String[] args) {
 		String sentence = sc.nextLine();
-		System.out.println("After Removing Spaces : "+removeSpaces(sentence));
-		System.out.println("After Removing Spaces : "+removeSpaces1(sentence));
+		System.out.println("After Removing Spaces : " + removeSpaces(sentence));
+		System.out.println("After Removing Spaces : " + removeSpaces1(sentence));
 	}
 
+// Reverse String Using Another String
 	public static String removeSpaces1(String sentence) {
 		String s = "";
-		for(int i= 0;i<sentence.length();i++)
-			if(sentence.charAt(i)!=' ')
-				s+=sentence.charAt(i);
+		for (int i = 0; i < sentence.length(); i++)
+			if (sentence.charAt(i) != ' ')
+				s += sentence.charAt(i);
 		return s;
 	}
 
+	// Reverse String Using Method
 	public static String removeSpaces(String sentence) {
 		return sentence.replace(" ", "");
 	}
