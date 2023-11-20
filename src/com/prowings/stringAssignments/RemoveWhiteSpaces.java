942d0727ef8859bd09c1e95cca31ebd330abd26b
Atul-Patil <atulpatil051099@gmail.com>
2023-11-20 10:22:42 +0530

    Remove Soaces

--- src/com/prowings/stringAssignments/RemoveWhiteSpaces.java
+++ src/com/prowings/stringAssignments/RemoveWhiteSpaces.java
@@ -9,11 +9,18 @@ public class RemoveWhiteSpaces {
 	public static void main(String[] args) {
 		String sentence = sc.nextLine();
 		System.out.println("After Removing Spaces : "+removeSpaces(sentence));
+		System.out.println("After Removing Spaces : "+removeSpaces1(sentence));
+	}
 
+	public static String removeSpaces1(String sentence) {
+		String s = "";
+		for(int i= 0;i<sentence.length();i++)
+			if(sentence.charAt(i)!=' ')
+				s+=sentence.charAt(i);
+		return s;
 	}
 
 	public static String removeSpaces(String sentence) {
-		
-		return null;
+		return sentence.replace(" ", "");
 	}
 }
