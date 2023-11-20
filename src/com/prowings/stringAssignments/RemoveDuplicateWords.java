123d753674cf1c53469cef090123c6c8f4259af3
Atul-Patil <atulpatil051099@gmail.com>
2023-11-20 10:38:59 +0530

    Remove Duplicate Characters

---
+++ src/com/prowings/stringAssignments/RemoveDuplicateWords.java
@@ -0,0 +1,26 @@
+package com.prowings.stringAssignments;
+
+import java.util.Scanner;
+
+public class RemoveDuplicateWords {
+
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		String word = sc.next();
+		System.out.println(removeDuplicates(word));
+	}
+
+	public static String removeDuplicates(String word) {
+		char[] ch = word.toCharArray();
+		for (int i = 0; i < ch.length; i++) {
+			for (int j = i + 1; j < ch.length; j++) {
+				if (word.charAt(i) == ch[j])
+					ch[j] = ' ';
+			}
+		}
+		String s = new String(ch);
+		s = s.replace(" ", "");
+		return s;
+	}
+}
